d0f8a8a6db6f47eb03cc949e149e7ab3bb2a487b
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-03 16:00:48 -0300

    Functions for getting Simpsons

--- node-js-day1/Simpsons/simpsons.js
+++ node-js-day1/Simpsons/simpsons.js
@@ -1,11 +1,23 @@
 const fs = require('fs').promises;
 
+async function getSimpsonById(id) {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  const chosenSimp = simpsons.find((simp) => Number(simp.id) === id);
+  if (!chosenSimp) throw new Error('id não encontrado');
+  return chosenSimp;
+}
+
 async function readAll() {
   const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  const strings = simpsons.map(({ id, name }) => `${id} - ${name}`);
+  strings.forEach((string) => console.log(string));
 }
 
 async function main() {
-  await readAll();
+  const simpson = await getSimpsonById(1);  
+  console.log(simpson);
 }
 
 main();

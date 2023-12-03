eca0867544a8838ab42e329ab1a3c8faf4d9040c
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-03 14:45:51 -0300

    IMC function and npm run bmi command

--- node-js-day1/bmi.js
+++ node-js-day1/bmi.js
@@ -1,2 +1,16 @@
-const weightInKg = 100;
+const weightInKg = 98;
 const heightInCm = 160;
+
+function handleBMI(weight, height) {
+  const heightInMeter = height / 100;
+  const heightSquared = heightInMeter ** 2;
+  const bmi = weight / heightSquared;
+  return bmi;
+}
+
+function main() {
+  const bmi = handleBMI(weightInKg, heightInCm);
+  console.log( `BMI: ${bmi.toFixed(2)}`);
+}
+
+main();

7a7fa395a583785103759d62d4baa346909cc4e0
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-04 13:25:04 -0300

    readFile function

--- node-js-day2/src/app.js
+++ node-js-day2/src/app.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
+
+const moviesPath = path.resolve(__dirname, 'movies.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(moviesPath, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    console.error(`Arquivo não lido: ${err}`);
+  }
+};
 
 module.exports = app;

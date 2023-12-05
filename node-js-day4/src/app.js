c877c5c2447def6aaee526983ac73b4f325d8e8c
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-05 13:38:25 -0300

    Middleware for name validation

--- node-js-day4/src/app.js
+++ node-js-day4/src/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const validateName = require('./middlewares/validateName');
 
 const app = express();
+app.use(express.json());
 
+app.post('/activities', validateName, (_req, res) => {
+  res.status(201).json({ message: 'Activity created successfully!' });
+});
 module.exports = app;

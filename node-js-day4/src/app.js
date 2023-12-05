854f577b80baf05d6d1dc1e77fa7dfd7181906cb
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-05 13:47:57 -0300

    Middleware for price validation

--- node-js-day4/src/app.js
+++ node-js-day4/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const validateName = require('./middlewares/validateName');
+const validatePrice = require('./middlewares/validadePrice');
 
 const app = express();
 app.use(express.json());
 
-app.post('/activities', validateName, (_req, res) => {
+app.post('/activities', validateName, validatePrice, (_req, res) => {
   res.status(201).json({ message: 'Activity created successfully!' });
 });
 module.exports = app;

874d3b9c7d6151a716362ba269611e9132c08b7a
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-05 14:06:11 -0300

    Middleware for createdAt validation

--- node-js-day4/src/app.js
+++ node-js-day4/src/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
 
 const app = express();
 app.use(express.json());
 
-app.post('/activities', validateDescription, validateName, validatePrice, (_req, res) => {
+app.post('/activities',
+validateCreatedAt,
+validateDescription,
+validateName,
+validatePrice,
+(_req, res) => {
   res.status(201).json({ message: 'Activity created successfully!' });
 });
 module.exports = app;

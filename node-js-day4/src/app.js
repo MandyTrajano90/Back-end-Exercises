b64d2f8a2f93d7e30c3b9bfcd091ba3549924eea
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-05 14:45:31 -0300

    Middlewares for /signup - token validation

--- node-js-day4/src/app.js
+++ node-js-day4/src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const validateName = require('./middlewares/validateName');
-const validatePrice = require('./middlewares/validatePrice');
-const validateDescription = require('./middlewares/validateDescription');
-const validateCreatedAt = require('./middlewares/validateCreatedAt');
-const validateRating = require('./middlewares/validateRating');
 const validateDifficulty = require('./middlewares/validateDifficulty');
+const validateRating = require('./middlewares/validateRating');
+const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateDescription = require('./middlewares/validateDescription');
+const validatePrice = require('./middlewares/validatePrice');
+const validateName = require('./middlewares/validateName');
 
 const app = express();
 app.use(express.json());
@@ -19,4 +19,12 @@ validatePrice,
 (_req, res) => {
   res.status(201).json({ message: 'Activity created successfully!' });
 });
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+});
+
 module.exports = app;

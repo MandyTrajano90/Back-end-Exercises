3bb73a61797749586180f97c5f9d895d082ec189
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-05 13:59:31 -0300

    Middleware for description validation

--- node-js-day4/src/app.js
+++ node-js-day4/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const validateName = require('./middlewares/validateName');
-const validatePrice = require('./middlewares/validadePrice');
+const validatePrice = require('./middlewares/validatePrice');
+const validateDescription = require('./middlewares/validateDescription');
 
 const app = express();
 app.use(express.json());
 
-app.post('/activities', validateName, validatePrice, (_req, res) => {
+app.post('/activities', validateDescription, validateName, validatePrice, (_req, res) => {
   res.status(201).json({ message: 'Activity created successfully!' });
 });
 module.exports = app;

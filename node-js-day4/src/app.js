e43b65df2851e0a014a8ad33f9cae511d596370c
Mandytrajano90 <mandytrajano90@gmail.com>
2023-12-05 14:50:01 -0300

    Middleware for authorization

--- node-js-day4/src/app.js
+++ node-js-day4/src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const auth = require('./middlewares/auth');
 const validateDifficulty = require('./middlewares/validateDifficulty');
 const validateRating = require('./middlewares/validateRating');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateDescription = require('./middlewares/validateDescription');
 const validatePrice = require('./middlewares/validatePrice');
 const validateName = require('./middlewares/validateName');
+const generateToken = require('./utils/generateToken');
 
 const app = express();
 app.use(express.json());
 
 app.post('/activities',
+auth,
 validateDifficulty,
 validateRating,
 validateCreatedAt,
@@ -25,6 +28,8 @@ app.post('/signup', (req, res) => {
   if ([email, password, firstName, phone].includes(undefined)) {
     return res.status(401).json({ message: 'Campos ausentes!' });
   }
+  const tokenGenerated = generateToken();
+  return res.status(200).json({ token: tokenGenerated });
 });
 
 module.exports = app;
